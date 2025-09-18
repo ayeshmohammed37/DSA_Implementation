430c26c79aeb6add5880187f10ef8f96a1bc1666
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-09-19 01:27:50 +0300

    Implement MyArray.Add

---
+++ DSA_Implementation/ArrayImplementation/MyArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_Implementation.ArrayImplementation
+{
+
+    /*
+     * Core Functionality:
+     *  - Add(item), Get(item)Indexer, RemoveAt(i), Count
+     * 
+     * 
+     * 
+     * 
+     */
+
+    // Dynamic Array
+    public class MyArray
+    {
+        int[] data;
+        int size;
+        int index;
+
+        public MyArray()
+        {
+            data = new int[4];
+            size = 4;
+            index = 0;
+        }
+
+        public void Add(int value)
+        {
+            if (index >= size)
+            {
+                extend();
+            }
+
+            data[index++] = value;
+        }
+
+        void extend()
+        {
+            int[] temp = new int[size + 4];
+
+            for (int i = 0; i < size;  i++)
+            {
+                temp[i] = data[i];
+            }
+            data = temp;
+            size += 4;
+        }
+    }
+}
--- DSA_Implementation/Program.cs
+++ DSA_Implementation/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace DSA_Implementation
+﻿using DSA_Implementation.ArrayImplementation;
+
+namespace DSA_Implementation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyArray arr = new MyArray();
+
+            arr.Add(1);
+            arr.Add(2);
         }
     }
 }
